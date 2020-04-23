ba6a9d027180955b5ff0b6de719dd345f434849c
lior itach <lioritach@yahoo.com>
2020-04-23 23:50:04 +0300

    Add Config package

---
+++ app/src/main/java/com/example/group21/Config/Config.java
@@ -0,0 +1,8 @@
+package com.example.group21.Config;
+
+public class Config {
+
+    public static final String PAYPAL_CLIENT_ID = "AXlDyKWah-LjInxag4XLAUKRKBUFvh02NEC1Lkjv-5ETDXLyLKIVO3UF8wW_JlW-CXauf4n7fnfu3n8V";
+
+
+}
