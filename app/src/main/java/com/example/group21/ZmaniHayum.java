9b6f733ead7e908a80164e972a80bfd9cf74907e
Tzuriel Yeffet <tzuriel66ye@yahoo.com>
2020-04-27 15:42:55 +0300

    add ZmaniHayom icon PM2020T21-55

---
+++ app/src/main/java/com/example/group21/ZmaniHayum.java
@@ -0,0 +1,19 @@
+package com.example.group21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class ZmaniHayum extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zmani_hayum);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.loadUrl("https://calendar.2net.co.il/todaytimes.aspx");
+    }
+}
