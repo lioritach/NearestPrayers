626ba208721986d46d40da671c627823b5ef7fcf
liranba1 <45334346+liranba1@users.noreply.github.com>
2020-05-03 13:12:10 +0300

    add HalachaYomit PM2020T21-58

---
+++ app/src/main/java/com/example/group21/HalachaYomit.java
@@ -0,0 +1,20 @@
+package com.example.group21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class HalachaYomit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halacha_yomit);
+
+        WebView webView = new WebView(this);
+        setContentView(webView);
+        webView.loadUrl("http://halachayomit.co.il/");
+
+    }
+}
