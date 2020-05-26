523031fa1b22de8b0320803295fac2333e3f503e
Lior Itach <44543472+lioritach@users.noreply.github.com>
2020-05-26 23:35:56 +0300

    Update GabayRegister.java

--- app/src/main/java/com/example/group21/Authentication/GabayRegister.java
+++ app/src/main/java/com/example/group21/Authentication/GabayRegister.java
@@ -1,4 +1,4 @@
-package com.example.group21;
+package com.example.group21.Authentication;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -24,6 +24,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.group21.MainGabayActivity;
+import com.example.group21.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
