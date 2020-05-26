8d781a5f0da7faa0ade4157f4a880bea2c93da15
Lior Itach <44543472+lioritach@users.noreply.github.com>
2020-05-26 23:37:20 +0300

    Update LoginTest.java

--- app/src/androidTest/java/com/example/group21/LoginTest.java
+++ app/src/androidTest/java/com/example/group21/LoginTest.java
@@ -2,9 +2,12 @@ package com.example.group21;
 
 import android.app.Activity;
 import android.app.Instrumentation;
-import android.util.Log;
+import android.widget.EditText;
 
 import androidx.test.rule.ActivityTestRule;
+
+import com.example.group21.Authentication.GabayLogin;
+import com.example.group21.Authentication.Login;
 
 import org.junit.After;
 import org.junit.Before;
@@ -20,10 +23,10 @@ import static org.junit.Assert.*;
 public class LoginTest {
 
     @Rule
-    public ActivityTestRule<Login> loginActivityTestRule = new ActivityTestRule<Login>(Login.class);
-    private Login mlogin = null;
+    public ActivityTestRule<GabayLogin> loginActivityTestRule = new ActivityTestRule<GabayLogin>(GabayLogin.class);
+    private GabayLogin mlogin = null;
 
-    Instrumentation.ActivityMonitor monitorLogin = getInstrumentation().addMonitor(Login.class.getName(), null, false);
+    Instrumentation.ActivityMonitor monitorLogin = getInstrumentation().addMonitor(GabayLogin.class.getName(), null, false);
 
 
     @Before
@@ -33,12 +36,23 @@ public class LoginTest {
 
     /* Test for Login Button */
     @Test
-    public void testLoginActivity(){
+    public void testLoginButtonActivity(){
         assertNotNull(mlogin.findViewById(R.id.login_id));
         onView(withId(R.id.login_id)).perform(click());
         Activity Login = getInstrumentation().waitForMonitorWithTimeout(monitorLogin, 5000);
         assertNotNull(Login);
         Login.finish();
+    }
+
+    @Test
+    public void loginTestEditText() {
+
+        EditText registerPassword = mlogin.findViewById(R.id.password);
+        EditText registerEmail = mlogin.findViewById(R.id.Email);
+
+        //Check if the name, email, phone are not null
+        assertNotNull(registerPassword);
+        assertNotNull(registerEmail);
 
     }
 
