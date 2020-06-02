2c8cc853dd2747a905d653f5c5f0bd840a1722bb
nitay <nitaai.maalka@gmail.com>
2020-06-02 13:05:24 +0300

    add data to menu

---
+++ app/src/main/java/com/example/group21/Constants.java
@@ -0,0 +1,21 @@
+package com.example.group21;
+
+public class Constants {
+
+    public static final String[] options = {
+            "נוסח החזן",
+            "נוסח ספרדי",
+            "נוסח אשכנזי",
+            "נוסח תימני",
+            "נוסח האר'י"
+    };
+
+    public static final String[] options1 = {
+            "הכל",
+            "נוסח החזן",
+            "נוסח ספרדי",
+            "נוסח אשכנזי",
+            "נוסח תימני",
+            "נוסח האר'י"
+    };
+}
